6ef86f38e73b6a9d22c54b8c59e49b620eedf55a
mihailone <38872456+mihailone@users.noreply.github.com>
2018-05-11 20:19:12 +0300

    1

---
+++ script/popup.js
@@ -0,0 +1,51 @@
+function anichange1 (objName) {
+ if ( $(objName).css('display') == 'none' ) {
+ $(objName).animate({height: 'show'}, 400);
+ } else {
+ $(objName).animate({height: 'hide'}, 200);
+ }
+}
+function anichange2(objName) {
+    if ($(objName).css('display') == 'none') {
+        $(objName).animate({
+            height: 'show'
+        }, 400);
+    } else {
+        $(objName).animate({
+            height: 'hide'
+        }, 200);
+    }
+}
+function anichange3(objName) {
+    if ($(objName).css('display') == 'none') {
+        $(objName).animate({
+            height: 'show'
+        }, 400);
+    } else {
+        $(objName).animate({
+            height: 'hide'
+        }, 200);
+    }
+}
+function anichange4(objName) {
+    if ($(objName).css('display') == 'none') {
+        $(objName).animate({
+            height: 'show'
+        }, 400);
+    } else {
+        $(objName).animate({
+            height: 'hide'
+        }, 200);
+    }
+}
+function anichange5(objName) {
+    if ($(objName).css('display') == 'none') {
+        $(objName).animate({
+            height: 'show'
+        }, 400);
+    } else {
+        $(objName).animate({
+            height: 'hide'
+        }, 200);
+    }
+}
